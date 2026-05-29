2a2146c7a0f0a92de5ec84dbf09b1847efd90c87
aandrx <andrewliucodes@gmail.com>
2026-05-28 20:27:52 -0400

    update host address

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def start_bot():
 async def main():
     bot_task = asyncio.create_task(start_bot())
     server = uvicorn.Server(
-        uvicorn.Config(app, host="127.0.0.1", port=7777, log_level="info")
+        uvicorn.Config(app, host="0.0.0.0", port=7777, log_level="info")
     )
     server_task = asyncio.create_task(server.serve())
     await asyncio.gather(bot_task, server_task)
